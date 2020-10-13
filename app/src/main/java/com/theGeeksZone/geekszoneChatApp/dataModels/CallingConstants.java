c32ddf0d9d09fbd71ad7c17fdaab702f07295003
Zeeshan Ali <zeeshanalik73@gmail.com>
2020-10-13 16:44:27 +0500

    Done Something

---
+++ app/src/main/java/com/theGeeksZone/geekszoneChatApp/dataModels/CallingConstants.java
@@ -0,0 +1,13 @@
+package com.theGeeksZone.geekszoneChatApp.dataModels;
+
+public class CallingConstants {
+    public static final String CALL_TYPE_AUDIO="audio";
+    public static final String CALL_TYPE_VIDEO="video";
+
+    public static final String CALL_TYPE_KEY="call_type";
+    public static final String CALLER_UID_KEY="caller_uid";
+
+    public static final String RECEIVER_TOKEN_KEY = "receiver_fcm_token";
+    public static final String CALL_CANCELLED = "cancelled";
+    public static final String CALL_ACCEPTED = "accepted";
+}
